233ec4e48b6f06bb7ccefb88cdbf18886965d2c6
takashicompany <t@kashi.company>
2021-06-11 12:47:10 +0900

    実装をシンプルにした

--- PoolingContainer.cs
+++ PoolingContainer.cs
@@ -3,6 +3,7 @@
 
 	using UnityEngine;
 	using System.Collections.Generic;
+	using System.Linq;
 	
 	/// <summary>
 	/// GameObjectを再利用するクラス
@@ -20,9 +21,7 @@
 		[SerializeField]
 		protected Transform _container;
 
-		protected List<T> _list = new List<T>();
-
-		public List<T> list { get{ return _list; } }
+		protected List<T> _pooledList = new List<T>();
 
 		public void Setup(T prefab, Transform container)
 		{
@@ -35,7 +34,7 @@
 		/// </summary>
 		public void Prepare(int amount)
 		{
-			while(_list.Count < amount)
+			while(_pooledList.Count < amount)
 			{
 				Generate();
 			}
@@ -46,7 +45,7 @@
 		/// </summary>
 		public virtual void CollectAll()
 		{
-			foreach (var item in _list)
+			foreach (var item in _pooledList)
 			{
 				item.transform.SetParent(_container);
 				item.gameObject.SetActive(false);
@@ -58,10 +57,9 @@
 		/// </summary>
 		/// <param name="flag"></param>
 		/// <returns></returns>
-		protected T Find(bool flag)
+		protected T FindOne(bool flag)
 		{
-			if (_list == null) _list = new List<T>();
-			return _list.Find(m => m.gameObject.activeSelf == flag);
+			return _pooledList.Find(m => m.gameObject.activeSelf == flag);
 		}
 
 		/// <summary>
@@ -69,10 +67,15 @@
 		/// </summary>
 		/// <param name="flag"></param>
 		/// <returns></returns>
-		private List<T> FindList(bool flag)
+		private IEnumerable<T> FindAll(bool usable)
 		{
-			if (_list == null) _list = new List<T>();
-			return _list.FindAll(m => m.gameObject.activeSelf == flag);
+			foreach (var p in _pooledList)
+			{
+				if (CanUse(p) == usable)
+				{
+					yield return p;
+				}
+			}
 		}
 
 		/// <summary>
@@ -81,7 +84,7 @@
 		/// <returns></returns>
 		public virtual T Get()
 		{
-			var myObject = Find(false);
+			var myObject = FindOne(false);
 
 			if (myObject == null)
 			{
@@ -92,13 +95,14 @@
 			return myObject;
 		}
 
-		/// <summary>
-		/// アクティブなオブジェクトを取得する
-		/// </summary>
-		/// <returns></returns>
-		public List<T> GetActives()
+		public List<T> GetUsedList()
 		{
-			return FindList(true);
+			return FindAll(true).ToList();
+		}
+
+		public IEnumerable<T> GetUsedAll()
+		{
+			return FindAll(true);
 		}
 
 		/// <summary>
@@ -109,10 +113,10 @@
 		{
 			var myObject = GameObject.Instantiate(_prefab, _container);
 			myObject.gameObject.SetActive(false);
-			myObject.name = _prefab.name + "_" + _list.Count;
+			myObject.name = _prefab.name + "_" + _pooledList.Count;
 			myObject.transform.localPosition = Vector3.zero;
 			myObject.transform.localScale = _prefab.transform.localScale;
-			_list.Add(myObject);
+			_pooledList.Add(myObject);
 
 			return myObject;
 		}
@@ -122,181 +126,14 @@
 		/// </summary>
 		/// <param name="obj"></param>
 		/// <returns></returns>
-		public bool IsUsed(T obj)
+		public static bool IsUsed(T obj)
 		{
 			return obj.gameObject.activeSelf;
 		}
-	}
 
-	/// <summary>
-	/// GameObjectを再利用するコンテイナー
-	/// GameObject.activeSelf で再利用可/利用中を管理
-	/// </summary>
-	[System.Serializable]
-	public class BehaviourPoolingContainer<T> where T : Behaviour
-	{
-		[SerializeField]
-		private T _prefab;
-
-		public T prefab { get; private set; }
-
-		[SerializeField]
-		private Transform _container;
-
-		[SerializeField]
-		private bool _switchIsActive = true;
-
-		private List<T> _list = new List<T>();
-
-		public List<T> list { get{ return _list; } }
-
-		public void Setup(T prefab, Transform container, bool switchIsActive = true)
+		public static bool CanUse(T obj)
 		{
-			_prefab = prefab;
-			_container = container;
-			_switchIsActive = switchIsActive;
-		}
-
-		/// <summary>
-		/// 指定した個数までオブジェクトを生成しておく
-		/// </summary>
-		public void Prepare(int amount)
-		{
-			while(_list.Count < amount)
-			{
-				Generate();
-			}
-		}
-
-		/// <summary>
-		/// 全てのオブジェクトを回収する
-		/// </summary>
-		public virtual void CollectAll()
-		{
-			foreach (var item in _list)
-			{
-				item.transform.SetParent(_container);
-
-				if (_switchIsActive)
-				{
-					item.gameObject.SetActive(false);
-				}
-				else
-				{
-					item.enabled = false;
-				}
-			}
-		}
-
-		/// <summary>
-		/// activeがtrue/falseのオブジェクトを一つ取得する
-		/// </summary>
-		/// <param name="flag"></param>
-		/// <returns></returns>
-		private T Find(bool flag)
-		{
-			if (_list == null) _list = new List<T>();
-			
-			if (_switchIsActive)
-			{
-				return _list.Find(m => m.gameObject.activeSelf == flag);
-			}
-			else
-			{
-				return _list.Find(m => m.enabled == flag);
-			}
-		}
-
-		/// <summary>
-		/// activeがtrue/falseのオブジェクトリストを取得する
-		/// </summary>
-		/// <param name="flag"></param>
-		/// <returns></returns>
-		private List<T> FindList(bool flag)
-		{
-			if (_list == null) _list = new List<T>();
-
-			if (_switchIsActive)
-			{
-				return _list.FindAll(m => m.gameObject.activeSelf == flag);
-			}
-			else
-			{
-				return _list.FindAll(m => m.enabled == flag);
-			}
-		}
-
-		/// <summary>
-		/// オブジェクトを取得する
-		/// </summary>
-		/// <returns></returns>
-		public virtual T Get()
-		{
-			var myObject = Find(false);
-
-			if (myObject == null)
-			{
-				myObject = Generate();
-			}
-			if (_switchIsActive)
-			{
-				myObject.gameObject.SetActive(true);
-			}
-			else
-			{
-				myObject.enabled = true;
-			}
-			return myObject;
-		}
-
-		/// <summary>
-		/// アクティブなオブジェクトを取得する
-		/// </summary>
-		/// <returns></returns>
-		public List<T> GetActives()
-		{
-			return FindList(true);
-		}
-
-		/// <summary>
-		/// オブジェクトを生成する
-		/// </summary>
-		/// <returns></returns>
-		protected virtual T Generate()
-		{
-			var myObject = GameObject.Instantiate(_prefab, _container);
-
-			if (_switchIsActive)
-			{
-				myObject.gameObject.SetActive(false);
-			}
-			else
-			{
-				myObject.enabled = false;
-			}
-			myObject.name = _prefab.name + "_" + _list.Count;
-			myObject.transform.localPosition = Vector3.zero;
-			myObject.transform.localScale = _prefab.transform.localScale;
-			_list.Add(myObject);
-
-			return myObject;
-		}
-
-		/// <summary>
-		/// オブジェクトが利用されているか
-		/// </summary>
-		/// <param name="obj"></param>
-		/// <returns></returns>
-		public bool IsUsed(T obj)
-		{
-			if (_switchIsActive)
-			{
-				return obj.gameObject.activeSelf;
-			}
-			else
-			{
-				return obj.enabled;
-			}
+			return !IsUsed(obj);
 		}
 	}
 }
